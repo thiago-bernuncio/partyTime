3bb58f00bcdf377625a64eed2cf9ce96477b601d
Thiago Bernuncio <thiago.bernuncio@gmail.com>
2024-04-14 13:30:59 -0300

    Inserindo os dados no banco

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -2,6 +2,6 @@ const router = require("express").Router()
 
 const serviceController = require("../controllers/serviceController")
 
-// Funções 
+router.route("/services").post((req, res) => serviceController.create(req, res))
 
 module.exports = router
